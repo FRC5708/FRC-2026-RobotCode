4d7ee51e57c3976a00fcde91e33716aa4fc3714b
Jesse Kane <jessekane66@gmail.com>
2026-03-04 17:16:22 -0500

    stole constants from 1076

--- FRC-2026-RobotCode/src/main/java/frc/robot/Constants.java
+++ FRC-2026-RobotCode/src/main/java/frc/robot/Constants.java
@@ -8,9 +8,11 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import java.util.AbstractMap;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.util.Units;
+import frc.robot.subsystems.vision.VisionConstants;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -78,7 +80,7 @@ public final class Constants {
     public static final int canIDHood = 30;
     //0.020 is one tick
     public static final double shootWindUp = .3;
-    public static final double flywheelRadiusMeters = 0.1; // Dummy value
+    public static final double flywheelRadiusMeters = Units.inchesToMeters(2); // Dummy value
 
 
     //dummy values right now
@@ -101,6 +103,72 @@ public final class Constants {
   public static class FieldConstants {
     public static class PosesOfInterest {
       public static final Pose2d redHub = new Pose2d(Units.inchesToMeters(483.11),Units.inchesToMeters(135.09),new Rotation2d(0));
+    }
+
+    public static final double fieldLength = VisionConstants.kFieldLayout.getFieldLength();
+    public static final double fieldWidth = VisionConstants.kFieldLayout.getFieldWidth();
+
+
+    /** Hub related constants */
+    public static class Hub {
+
+    // Dimensions
+      public static final double width = Units.inchesToMeters(47.0);
+      public static final double height =
+        Units.inchesToMeters(72.0); // includes the catcher at the top
+      public static final double innerWidth = Units.inchesToMeters(41.7);
+      public static final double innerHeight = Units.inchesToMeters(56.5);
+
+      // Relevant reference points on alliance side
+      public static final Translation3d topCenterPoint =
+        new Translation3d(
+          VisionConstants.kFieldLayout.getTagPose(26).get().getX() + width / 2.0,
+            fieldWidth / 2.0,
+            height);
+      public static final Translation3d innerCenterPoint =
+        new Translation3d(
+          VisionConstants.kFieldLayout.getTagPose(26).get().getX() + width / 2.0,
+            fieldWidth / 2.0,
+            innerHeight);
+
+      public static final Translation2d nearLeftCorner =
+        new Translation2d(topCenterPoint.getX() - width / 2.0, fieldWidth / 2.0 + width / 2.0);
+      public static final Translation2d nearRightCorner =
+        new Translation2d(topCenterPoint.getX() - width / 2.0, fieldWidth / 2.0 - width / 2.0);
+      public static final Translation2d farLeftCorner =
+        new Translation2d(topCenterPoint.getX() + width / 2.0, fieldWidth / 2.0 + width / 2.0);
+      public static final Translation2d farRightCorner =
+        new Translation2d(topCenterPoint.getX() + width / 2.0, fieldWidth / 2.0 - width / 2.0);
+
+      // Relevant reference points on the opposite side
+      public static final Translation3d oppTopCenterPoint =
+        new Translation3d(
+          VisionConstants.kFieldLayout.getTagPose(4).get().getX() + width / 2.0,
+            fieldWidth / 2.0,
+            height);
+      public static final Translation3d oppInnerCenterPoint =
+        new Translation3d(
+          VisionConstants.kFieldLayout.getTagPose(4).get().getX() + width / 2.0,
+            fieldWidth / 2.0,
+            innerHeight);
+      public static final Translation2d oppNearLeftCorner =
+        new Translation2d(oppTopCenterPoint.getX() - width / 2.0, fieldWidth / 2.0 + width / 2.0);
+      public static final Translation2d oppNearRightCorner =
+        new Translation2d(oppTopCenterPoint.getX() - width / 2.0, fieldWidth / 2.0 - width / 2.0);
+      public static final Translation2d oppFarLeftCorner =
+        new Translation2d(oppTopCenterPoint.getX() + width / 2.0, fieldWidth / 2.0 + width / 2.0);
+      public static final Translation2d oppFarRightCorner =
+        new Translation2d(oppTopCenterPoint.getX() + width / 2.0, fieldWidth / 2.0 - width / 2.0);
+
+      // Hub faces
+      public static final Pose2d nearFace =
+        VisionConstants.kFieldLayout.getTagPose(26).get().toPose2d();
+      public static final Pose2d farFace =
+        VisionConstants.kFieldLayout.getTagPose(20).get().toPose2d();
+      public static final Pose2d rightFace =
+        VisionConstants.kFieldLayout.getTagPose(18).get().toPose2d();
+      public static final Pose2d leftFace =
+        VisionConstants.kFieldLayout.getTagPose(21).get().toPose2d();
     }
   }
 }
