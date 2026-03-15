4d06a05688d262fbd9629710c36a02d581727139
Jesse Kane <jessekane66@gmail.com>
2026-03-15 16:35:56 -0400

    new runIntake command that is debounced

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -5,19 +5,25 @@
 package frc.robot.subsystems;
 
 import frc.robot.Constants.Intake;
+import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-//import com.revrobotics.RelativeEncoder;
+import com.revrobotics.RelativeEncoder;
 //import edu.wpi.first.wpilibj.DigitalInput;
 
 public class IntakeSubsystem extends SubsystemBase {
   private SparkMax m_deploy = new SparkMax(Intake.canIDDeploy, MotorType.kBrushless);
   private SparkMax m_intake = new SparkMax(Intake.canIDIntake, MotorType.kBrushless);
-  //private RelativeEncoder m_intake_encoder = m_intake.getEncoder();
+  private RelativeEncoder m_intake_encoder = m_intake.getEncoder();
+  public static final double homingDebounceTime = 0.25;
+  public static final double homingVelocityThreshold = 0.1;
+  private Debouncer homingDebouncer;
+  private boolean homed;
   //private RelativeEncoder m_deploy_encoder = m_deploy.getEncoder();
   ///private DigitalInput forwardCheck = new DigitalInput(0);
   //private DigitalInput backwardCheck = new DigitalInput(1);
@@ -61,4 +67,20 @@ public class IntakeSubsystem extends SubsystemBase {
     deployCurrent.setDouble(m_deploy.getOutputCurrent());
     deployVoltage.setDouble(m_deploy.get());
   }
+
+  //ts is actually so cooked ong. Yes i am too lazy to change the varuiable names
+   public Command runIntake(double power) {
+        return startRun(
+            () -> {
+                homingDebouncer = new Debouncer(homingDebounceTime);
+                homingDebouncer.calculate(false);
+                intake(power);
+            },
+            () -> {
+                homed = homingDebouncer.calculate(Math.abs(m_intake_encoder.getVelocity()) <= homingVelocityThreshold);
+            }
+        )
+        .until(() -> homed)
+        .andThen(() -> intake(0));
+    }
 }
